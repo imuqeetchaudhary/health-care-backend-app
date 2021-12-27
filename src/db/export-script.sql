6cce128965f2834390321f8447965ac904e4c27c
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2021-12-27 15:37:17 +0500

    added db script of previously created models

--- src/db/export-script.sql
+++ src/db/export-script.sql
@@ -126,6 +126,180 @@ CREATE TABLE `ad_user_access_roles` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appointment` (
+  `appointment_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `appointment_date_time` datetime NOT NULL,
+  `appointment_reason` varchar(255) NOT NULL,
+  `fee` bigint unsigned NOT NULL,
+  `patient_id` bigint unsigned NOT NULL,
+  `doctor_id` bigint unsigned NOT NULL,
+  `disease_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`appointment_id`),
+  KEY `fk_appointment_created_by_idx` (`created_by`),
+  KEY `fk_appointment_updated_by_idx` (`updated_by`),
+  KEY `fk_appointment_patient_idx` (`patient_id`),
+  KEY `fk_appointment_doctor_idx` (`doctor_id`),
+  KEY `fk_appointment_disease_idx` (`disease_id`),
+  CONSTRAINT `fk_appointment_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_appointment_disease` FOREIGN KEY (`disease_id`) REFERENCES `disease` (`disease_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_appointment_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_appointment_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_appointment_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `department` (
+  `department_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `max_doctors` bigint unsigned NOT NULL,
+  `max_patients` bigint unsigned NOT NULL,
+  `phone_no` varchar(55) NOT NULL,
+  `email` varchar(55) NOT NULL,
+  `status` varchar(55) NOT NULL,
+  `hospital_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`department_id`),
+  UNIQUE KEY `department_name` (`department_name`),
+  KEY `fk_department_created_by_idx` (`created_by`),
+  KEY `fk_department_updated_by_idx` (`updated_by`),
+  KEY `fk_department_hopital_idx` (`hospital_id`),
+  CONSTRAINT `fk_department_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_department_hopital` FOREIGN KEY (`hospital_id`) REFERENCES `hospital` (`hospital_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_department_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `disease`
+--
+
+DROP TABLE IF EXISTS `disease`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disease` (
+  `disease_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `disease_name` varchar(255) NOT NULL,
+  `disease_description` varchar(255) NOT NULL,
+  `disease_symptoms` varchar(255) NOT NULL,
+  `disease_causes` varchar(255) NOT NULL,
+  `disease_type` varchar(255) NOT NULL,
+  `risk_factor` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`disease_id`),
+  KEY `fk_disease_created_by_idx` (`created_by`),
+  KEY `fk_disease_updated_by_idx` (`updated_by`),
+  CONSTRAINT `fk_disease_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_disease_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `disease_treated_by_dr`
+--
+
+DROP TABLE IF EXISTS `disease_treated_by_dr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disease_treated_by_dr` (
+  `dtbd_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `disease_id` bigint unsigned NOT NULL,
+  `doctor_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`dtbd_id`),
+  KEY `fk_dtbd_created_by_idx` (`created_by`),
+  KEY `fk_dtbd_updated_by_idx` (`updated_by`),
+  KEY `fk_dtbd_disease_idx` (`disease_id`),
+  KEY `fk_dtbd_doctor_idx` (`doctor_id`),
+  CONSTRAINT `fk_dtbd_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dtbd_disease` FOREIGN KEY (`disease_id`) REFERENCES `disease` (`disease_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dtbd_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dtbd_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor` (
+  `doctor_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `fee` bigint unsigned NOT NULL,
+  `experience` bigint unsigned NOT NULL,
+  `is_available` tinyint(1) NOT NULL,
+  `availablity_days` varchar(55) NOT NULL,
+  `active_hours` varchar(55) NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`doctor_id`),
+  KEY `fk_doctor_created_by_idx` (`created_by`),
+  KEY `fk_doctor_updated_by_idx` (`updated_by`),
+  KEY `fk_doctor_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_doctor_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_doctor_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_doctor_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dr_in_depart`
+--
+
+DROP TABLE IF EXISTS `dr_in_depart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dr_in_depart` (
+  `dr_in_depart_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `doctor_id` bigint unsigned NOT NULL,
+  `department_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`dr_in_depart_id`),
+  KEY `fk_dir_created_by_idx` (`created_by`),
+  KEY `fk_dir_created_by_idx1` (`updated_by`),
+  KEY `fk_dir_doctor_idx` (`doctor_id`),
+  KEY `fk_dir_department_idx` (`department_id`),
+  CONSTRAINT `fk_dir_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dir_department` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_dir_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `hospital`
 --
 
@@ -161,6 +335,31 @@ CREATE TABLE `hospital` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `medicine_history`
+--
+
+DROP TABLE IF EXISTS `medicine_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `medicine_history` (
+  `m_history_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `medicine_id` bigint unsigned NOT NULL,
+  `appointment_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`m_history_id`),
+  KEY `fk_mh_created_by_idx` (`created_by`),
+  KEY `fk_mh_updated_by_idx` (`updated_by`),
+  KEY `fk_mh_appointment_idx` (`appointment_id`),
+  CONSTRAINT `fk_mh_appointment` FOREIGN KEY (`appointment_id`) REFERENCES `appointment` (`appointment_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_mh_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_mh_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `menu_category`
 --
 
@@ -181,6 +380,165 @@ CREATE TABLE `menu_category` (
   CONSTRAINT `menu_category_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `menu_category_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient` (
+  `patient_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) NOT NULL,
+  `user_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`patient_id`),
+  KEY `fk_patient_created_by_idx` (`created_by`),
+  KEY `fk_patient_updated_by_idx` (`updated_by`),
+  KEY `fk_patient_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_patient_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_patient_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_patient_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient_in_depart`
+--
+
+DROP TABLE IF EXISTS `patient_in_depart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient_in_depart` (
+  `patient_in_depart_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `patient_id` bigint unsigned NOT NULL,
+  `department_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`patient_in_depart_id`),
+  KEY `fk_patient_created_by_idx` (`created_by`),
+  KEY `fk_patient_updated_by_idx` (`updated_by`),
+  KEY `fk_pid_patient_idx` (`patient_id`),
+  KEY `fk_pid_department_idx` (`department_id`),
+  CONSTRAINT `fk_pid_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_pid_department` FOREIGN KEY (`department_id`) REFERENCES `department` (`department_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_pid_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_pid_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `patient_previous_disease`
+--
+
+DROP TABLE IF EXISTS `patient_previous_disease`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient_previous_disease` (
+  `p_history_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `duaration_unit` varchar(255) NOT NULL,
+  `duaration_count` varchar(255) NOT NULL,
+  `patient_id` bigint unsigned NOT NULL,
+  `disease_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`p_history_id`),
+  KEY `fk_ppd_created_by_idx` (`created_by`),
+  KEY `fk_ppd_updated_by_idx` (`updated_by`),
+  KEY `fk_ppd_patient_idx` (`patient_id`),
+  KEY `fk_ppd_disease_idx` (`disease_id`),
+  CONSTRAINT `fk_ppd_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ppd_disease` FOREIGN KEY (`disease_id`) REFERENCES `disease` (`disease_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ppd_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ppd_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `qualification`
+--
+
+DROP TABLE IF EXISTS `qualification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `qualification` (
+  `qualification_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `details` varchar(55) NOT NULL,
+  `doctor_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`qualification_id`),
+  KEY `fk_qualification_created_by_idx` (`created_by`),
+  KEY `fk_qualification_updated_by_idx` (`updated_by`),
+  KEY `fk_qualification_doctor_idx` (`doctor_id`),
+  CONSTRAINT `fk_qualification_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_qualification_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_qualification_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `review_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `stars` bigint unsigned NOT NULL,
+  `patient_id` bigint unsigned NOT NULL,
+  `doctor_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `fk_review_created_by_idx` (`created_by`),
+  KEY `fk_review_updated_by_idx` (`updated_by`),
+  KEY `fk_review_patient_idx` (`patient_id`),
+  KEY `fk_review_doctor_idx` (`doctor_id`),
+  CONSTRAINT `fk_review_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_patient` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`patient_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_review_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `specialization`
+--
+
+DROP TABLE IF EXISTS `specialization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `specialization` (
+  `specialization_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `details` varchar(55) NOT NULL,
+  `doctor_id` bigint unsigned NOT NULL,
+  `created_by` bigint unsigned NOT NULL,
+  `updated_by` bigint unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`specialization_id`),
+  KEY `fk_specialization_created_by_idx` (`created_by`),
+  KEY `fk_specialization_updated_by_idx` (`updated_by`),
+  KEY `fk_specialization_doctor_idx` (`doctor_id`),
+  CONSTRAINT `fk_specialization_created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_specialization_doctor` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`doctor_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_specialization_updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -216,4 +574,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-23  1:56:57
+-- Dump completed on 2021-12-27 15:36:25
