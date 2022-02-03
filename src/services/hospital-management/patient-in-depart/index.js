05a33a58183abb0f67d7d026e95d523199abeb64
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-02-03 23:52:10 +0500

    added search on patientId instead of PID id

--- src/services/hospital-management/patient-in-depart/index.js
+++ src/services/hospital-management/patient-in-depart/index.js
@@ -2,159 +2,175 @@ const db = require("../../../models");
 const dbUtils = require("../../error-check.util");
 const Exceptions = require("../../../utils/custom-exceptions");
 
-exports.savePatientInDepart = async({
+exports.savePatientInDepart = async ({
+  departmentId,
+  patientId,
+  actionPerformedBy,
+}) => {
+  const patientInDepart = {
     departmentId,
     patientId,
-    actionPerformedBy,
-}) => {
-    const patientInDepart = {
-        departmentId,
-        patientId,
-        createdBy: actionPerformedBy,
-        updatedBy: actionPerformedBy,
-    };
+    createdBy: actionPerformedBy,
+    updatedBy: actionPerformedBy,
+  };
 
-    try {
-        const newPatientInDepart = await db.PatientInDepart.create({
-            ...patientInDepart,
-        });
-        return newPatientInDepart;
-    } catch (err) {
-        if (dbUtils.isRecordDuplicate(err)) {
-            throw new Exceptions.BaPatientequest({
-                message: "patient in department already exists",
-            });
-        }
-        throw err;
+  try {
+    const newPatientInDepart = await db.PatientInDepart.create({
+      ...patientInDepart,
+    });
+    return newPatientInDepart;
+  } catch (err) {
+    if (dbUtils.isRecordDuplicate(err)) {
+      throw new Exceptions.BaPatientequest({
+        message: "patient in department already exists",
+      });
     }
+    throw err;
+  }
 };
 
 exports.listAllPatientInDepart = () => {
-    return db.PatientInDepart.findAll({
-        attributes: ["patientInDepartId"],
-        include: [{
-                model: db.Department,
-                attributes: ["departmentId", "departmentName"],
-            },
-            {
-                model: db.Patient,
-                attributes: ["patientId", "status", "userId"],
-                include: [{
-                    model: db.User,
-                    attributes: [
-                        "userId",
-                        "email",
-                        "displayName",
-                        "isActive",
-                        "isAdmin",
-                        "isSuperuser",
-                        "city",
-                        "country",
-                        "postalCode",
-                        "area",
-                        "phoneNumber",
-                        "dob"
-                    ],
-                }, ],
-            },
+  return db.PatientInDepart.findAll({
+    attributes: ["patientInDepartId"],
+    include: [
+      {
+        model: db.Department,
+        attributes: ["departmentId", "departmentName"],
+      },
+      {
+        model: db.Patient,
+        attributes: ["patientId", "status", "userId"],
+        include: [
+          {
+            model: db.User,
+            attributes: [
+              "userId",
+              "email",
+              "displayName",
+              "isActive",
+              "isAdmin",
+              "isSuperuser",
+              "city",
+              "country",
+              "postalCode",
+              "area",
+              "phoneNumber",
+              "dob",
+            ],
+          },
         ],
-    });
+      },
+    ],
+  });
 };
 
 exports.findById = ({ id }) => {
-    return db.PatientInDepart.findByPk(id, {
-        attributes: ["patientInDepartId"],
-        include: [{
-                model: db.Department,
-                attributes: ["departmentId", "departmentName"],
-            },
+  return db.PatientInDepart.findByPk(
+    { where: { patientId: id } },
+    {
+      attributes: ["patientInDepartId"],
+      include: [
+        {
+          model: db.Department,
+          attributes: ["departmentId", "departmentName"],
+        },
+        {
+          model: db.Patient,
+          attributes: ["patientId", "status", "userId"],
+          include: [
             {
-                model: db.Patient,
-                attributes: ["patientId", "status", "userId"],
-                include: [{
-                    model: db.User,
-                    attributes: [
-                        "userId",
-                        "email",
-                        "displayName",
-                        "isActive",
-                        "isAdmin",
-                        "isSuperuser",
-                    ],
-                }, ],
+              model: db.User,
+              attributes: [
+                "userId",
+                "email",
+                "displayName",
+                "isActive",
+                "isAdmin",
+                "isSuperuser",
+              ],
             },
-        ],
-    });
+          ],
+        },
+      ],
+    }
+  );
 };
 
 exports.findByPid = ({ id }) => {
-    return db.PatientInDepart.findOne({
-        where: {
-            patientId: id
-        },
-        attributes: ["patientInDepartId"],
-        include: [{
-                model: db.Department,
-                attributes: ["departmentId", "departmentName"],
-            },
-            {
-                model: db.Patient,
-                attributes: ["patientId", "status", "userId"],
-                include: [{
-                    model: db.User,
-                    attributes: [
-                        "userId",
-                        "email",
-                        "displayName",
-                        "isActive",
-                        "isAdmin",
-                        "isSuperuser",
-                    ],
-                }, ],
-            },
+  return db.PatientInDepart.findOne({
+    where: {
+      patientId: id,
+    },
+    attributes: ["patientInDepartId"],
+    include: [
+      {
+        model: db.Department,
+        attributes: ["departmentId", "departmentName"],
+      },
+      {
+        model: db.Patient,
+        attributes: ["patientId", "status", "userId"],
+        include: [
+          {
+            model: db.User,
+            attributes: [
+              "userId",
+              "email",
+              "displayName",
+              "isActive",
+              "isAdmin",
+              "isSuperuser",
+            ],
+          },
         ],
-    });
+      },
+    ],
+  });
 };
 
-exports.updatePatientInDepart = async({
-    patientInDepartId,
+exports.updatePatientInDepart = async ({
+  patientInDepartId,
+  departmentId,
+  patientId,
+  actionPerformBy,
+}) => {
+  const patientInDepart = {
     departmentId,
     patientId,
-    actionPerformBy,
-}) => {
-    const patientInDepart = {
-        departmentId,
-        patientId,
-        updatedBy: actionPerformBy,
-    };
+    updatedBy: actionPerformBy,
+  };
 
-    try {
-        const updatedPatientInDepart = await db.PatientInDepart.update({...patientInDepart }, { where: { patientId: patientInDepartId } });
+  try {
+    const updatedPatientInDepart = await db.PatientInDepart.update(
+      { ...patientInDepart },
+      { where: { patientId: patientInDepartId } }
+    );
 
-        if (dbUtils.isRecordFound(updatedPatientInDepart))
-            throw new Exceptions.NotFound({
-                message: "patient in department not found",
-            });
-    } catch (err) {
-        if (dbUtils.isRecordDuplicate(err))
-            throw new Exceptions.BaPatientequest({
-                message: "patient in department already exists",
-            });
-        throw err;
-    }
+    if (dbUtils.isRecordFound(updatedPatientInDepart))
+      throw new Exceptions.NotFound({
+        message: "patient in department not found",
+      });
+  } catch (err) {
+    if (dbUtils.isRecordDuplicate(err))
+      throw new Exceptions.BaPatientequest({
+        message: "patient in department already exists",
+      });
+    throw err;
+  }
 };
 
-exports.deletePatientInDepart = async({ patientInDepartId }) => {
-    try {
-        const deletePatientInDepart = await db.PatientInDepart.destroy({
-            where: { patientInDepartId },
-        });
-    } catch (err) {
-        if (dbUtils.isFkFailed(err)) {
-            throw new Exceptions.BaPatientequest({
-                message: "Can't delete patient in department unless delete all its reference",
-            });
-        }
-        throw err;
+exports.deletePatientInDepart = async ({ patientInDepartId }) => {
+  try {
+    const deletePatientInDepart = await db.PatientInDepart.destroy({
+      where: { patientInDepartId },
+    });
+  } catch (err) {
+    if (dbUtils.isFkFailed(err)) {
+      throw new Exceptions.BaPatientequest({
+        message:
+          "Can't delete patient in department unless delete all its reference",
+      });
     }
+    throw err;
+  }
 };
